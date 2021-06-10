83e74d280ecfe84e2c88cd2283bf1bcadd3e5a60
yeohhq <yeohhuiqing00@gmail.com>
2021-06-10 16:44:11 +0800

    Remove redundant imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,46 +53,6 @@ class Header extends Component {
                 </h1>
               </div>
               <a role="button" class="btn btn-dark btn-lg" href="https://drive.google.com/file/d/1vvAdfC3pwiBfwZxKCHuVRXTs-Hy3zJ4w/view?usp=sharing">View my CV</a>
-              {/* <Switch
-                checked={this.state.checked}
-                onChange={this.onThemeSwitchChange}
-                offColor="#000000"
-                onColor="#353535"
-                className="react-switch mx-auto"
-                width={90}
-                height={40}
-                uncheckedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="twemoji:owl"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "20px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                checkedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="noto-v1:sun-with-face"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "10px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                id="icon-switch"
-              /> */}
             </div>
           </div>
         </div>
