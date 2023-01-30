2f5118dfbf7cc2b798b97e676a38c04fa3e16842
yeohhq <yeohhuiqing00@gmail.com>
2023-01-31 00:32:39 +0800

    Update portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,43 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class Footer extends Component {
-  render() {
-    if (this.props.sharedBasicInfo) {
-      var networks = this.props.sharedBasicInfo.social.map(function (network) {
-        return (
-          <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.class}></i>
-            </a>
-          </span>
-        );
-      });
-    }
+	render() {
+		if (this.props.sharedBasicInfo) {
+			var networks = this.props.sharedBasicInfo.social.map(function (
+				network
+			) {
+				return (
+					<span key={network.name} className="m-4">
+						<a
+							href={network.url}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<i className={network.class}></i>
+						</a>
+					</span>
+				)
+			})
+		}
 
-    return (
-      <footer>
-        <div className="col-md-12">
-        <h1 className="section-title" style={{ color: "white" }}>
-            <span>MY ART GALLERY</span>
-          </h1>
-          <div id="instafeed"></div>
-          <div className="social-links">{networks}</div>
-          <div className="copyright py-4 text-center">
-            <div className="container">
-              <small>
-                Copyright &copy;{" "}
-                {this.props.sharedBasicInfo
-                  ? this.props.sharedBasicInfo.name
-                  : "???"}
-              </small>
-            </div>
-          </div>
-        </div>
-      </footer>
-    );
-  }
+		return (
+			<footer>
+				<div className="col-md-12">
+					<div className="social-links">{networks}</div>
+					<div className="copyright py-4 text-center">
+						<div className="container">
+							<small>
+								Copyright &copy;{' '}
+								{this.props.sharedBasicInfo
+									? this.props.sharedBasicInfo.name
+									: '???'}
+							</small>
+						</div>
+					</div>
+				</div>
+			</footer>
+		)
+	}
 }
 
-export default Footer;
+export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,85 +1,104 @@
-import React, { Component } from "react";
-import Typical from "react-typical";
+import React, { Component } from 'react'
+import Typical from 'react-typical'
 
 class Header extends Component {
-  titles = [];
+	titles = []
 
-  constructor() {
-    super();
-    this.state = { checked: false };
-    this.onThemeSwitchChange = this.onThemeSwitchChange.bind(this);
-  }
+	constructor() {
+		super()
+		this.state = { checked: false }
+		this.onThemeSwitchChange = this.onThemeSwitchChange.bind(this)
+	}
 
-  onThemeSwitchChange(checked) {
-    this.setState({ checked });
-    this.setTheme();
-  }
+	onThemeSwitchChange(checked) {
+		this.setState({ checked })
+		this.setTheme()
+	}
 
-  setTheme() {
-    var dataThemeAttribute = "data-theme";
-    var body = document.body;
-    var newTheme =
-      body.getAttribute(dataThemeAttribute) === "dark" ? "light" : "dark";
-    body.setAttribute(dataThemeAttribute, newTheme);
-  }
+	setTheme() {
+		var dataThemeAttribute = 'data-theme'
+		var body = document.body
+		var newTheme =
+			body.getAttribute(dataThemeAttribute) === 'dark' ? 'light' : 'dark'
+		body.setAttribute(dataThemeAttribute, newTheme)
+	}
 
-  render() {
-    if (this.props.sharedData) {
-      var name = this.props.sharedData.name;
-      this.titles = this.props.sharedData.titles
-        .map((x) => [x.toUpperCase(), 1500])
-        .flat();
-    }
+	render() {
+		if (this.props.sharedData) {
+			var name = this.props.sharedData.name
+			this.titles = this.props.sharedData.titles
+				.map((x) => [x.toUpperCase(), 1500])
+				.flat()
 
-    const HeaderTitleTypeAnimation = React.memo(
-      () => {
-        return (
-          <Typical className="title-styles" steps={this.titles} loop={50} />
-        );
-      },
-      (props, prevProp) => true
-    );
+			var networks = this.props.sharedData.social.map(function (network) {
+				return (
+					<span key={network.name} className="m-4">
+						<a
+							href={network.url}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<i className={network.class}></i>
+						</a>
+					</span>
+				)
+			})
+		}
 
-    return (
-      <header
-        id="home"
-        style={{ height: window.innerHeight - 400, display: "block" }}
-      >
-        <div className="row aligner" style={{ height: "100%" }}>
-          <div className="col-md-12">
-            <div>
-              <br />
-              <h1 className="mb-0">
-                <Typical steps={[name]} wrapper="p" />
-              </h1>
-              <div className="title-container">
-                <HeaderTitleTypeAnimation />
-              </div>
-              <a
-                style={{ marginRight: "3rem" }}
-                role="button"
-                className="btn btn-dark btn-lg"
-                href="https://drive.google.com/drive/folders/1AkMZifpTEy_XGhDkzsV2pjbVY7M1la6K?usp=sharing"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                View my CV
-              </a>
-              <a
-                role="button"
-                className="btn btn-dark btn-lg"
-                href="https://www.linkedin.com/in/yeohhuiqing/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LinkedIn Profile
-              </a>
-            </div>
-          </div>
-        </div>
-      </header>
-    );
-  }
+		const HeaderTitleTypeAnimation = React.memo(
+			() => {
+				return (
+					<Typical
+						className="title-styles"
+						steps={this.titles}
+						loop={50}
+					/>
+				)
+			},
+			(props, prevProp) => true
+		)
+
+		return (
+			<header
+				id="home"
+				style={{ height: window.innerHeight - 400, display: 'block' }}
+			>
+				<div className="row aligner" style={{ height: '100%' }}>
+					<div className="col-md-12">
+						<div>
+							<br />
+							<h1 className="mb-0">
+								<Typical steps={[name]} wrapper="p" />
+							</h1>
+							<div className="title-container">
+								<HeaderTitleTypeAnimation />
+							</div>
+							<a
+								style={{ marginRight: '3rem' }}
+								role="button"
+								className="btn btn-dark btn-lg"
+								href="https://drive.google.com/drive/folders/1AkMZifpTEy_XGhDkzsV2pjbVY7M1la6K?usp=sharing"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								View my CV
+							</a>
+							<a
+								role="button"
+								className="btn btn-dark btn-lg"
+								href="https://www.linkedin.com/in/yeohhuiqing/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								LinkedIn Profile
+							</a>
+							<div className="social-links">{networks}</div>
+						</div>
+					</div>
+				</div>
+			</header>
+		)
+	}
 }
 
-export default Header;
+export default Header
